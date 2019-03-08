41cb39d6ec7efa345446bbb3781280f4ca860412
Camilo Lopez <camilino123@gmail.com>
2019-03-07 20:18:30 -0500

    Project initialized

---
+++ src/Handler/Demo.hs
@@ -0,0 +1,9 @@
+ {-# LANGUAGE NoImplicitPrelude #-}
+ {-# LANGUAGE OverloadedStrings #-}
+ {-# LANGUAGE TemplateHaskell #-}
+ {-# LANGUAGE MultiParamTypeClasses #-}
+ {-# LANGUAGE TypeFamilies #-}
+ module Handler.Demo where
+
+ import Import
+ import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
